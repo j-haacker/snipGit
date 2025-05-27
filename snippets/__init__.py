370428eae0d1e37fbdd6ddd362eada5b8f00a861
jan <152862650+j-haacker@users.noreply.github.com>
2025-05-27 22:08:12 +0200

    add wrap_sys_exit (not tested)

--- snippets/__init__.py
+++ snippets/__init__.py
@@ -7,5 +7,25 @@ __all__ = [
     "monkeypatch",
     "unsupervised",
     "parallel",
+    "wrap_sys_exit",
     "xr_utils",
 ]
+
+from contextlib import contextmanager
+from sys import exit
+
+
+@contextmanager
+def wrap_sys_exit(code_dict: set[tuple[Exception,int]] = None):
+    try:
+        yield
+    except Exception as err:
+        if code_dict is None:
+            exit(1)
+        else:
+            for _exception, exit_code in code_dict:
+                if isinstance(err, _exception):
+                    exit(exit_code)
+            exit(1)
+    else:
+        exit(0)
