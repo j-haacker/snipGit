125d2ed49af38602cd1b597c178ad1318d505916
jan <152862650+j-haacker@users.noreply.github.com>
2025-07-17 17:08:13 +0200

    add xr chunk selection

--- snippets/xr_utils.py
+++ snippets/xr_utils.py
@@ -1,7 +1,12 @@
 __all__ = [
     "build_enc_dict__cmpr_f4",
+    "get_chunk_number",
+    "sel_chunks_by_coord_range",
+    "sel_chunks_by_number_range",
 ]
 
+from collections.abc import Iterable
+import numpy as np
 import xarray as xr
 
 
@@ -11,3 +16,24 @@ def build_enc_dict__cmpr_f4(ds: xr.Dataset) -> dict:
         for _var in ds.data_vars
         if ds[_var].ndim > 1
     }
+
+
+def get_chunk_number(ds, dim, coords):
+    def _inner(coord):
+        if coord < ds[dim][0] or coord > ds[dim][-1]:
+            return None
+        return (ds[dim].isel({dim: np.cumsum(ds.chunks[dim]) - 1}) <= coord).argmin().item(0)
+    if isinstance(coords, Iterable):
+        return [_inner(coord) for coord in coords]
+    return _inner(coords)
+
+
+def sel_chunks_by_number_range(ds, dim, start, stop):
+    chunk_borders = np.cumsum([0] + list(ds.chunks[dim]))
+    return ds.isel({dim: slice(*chunk_borders[[start, stop + 1]])})
+
+
+def sel_chunks_by_coord_range(ds, **dim_intervals):
+    for dim, interval in dim_intervals.items():
+        ds = sel_chunks_by_number_range(ds, dim, *get_chunk_number(ds, dim, interval))
+    return ds
