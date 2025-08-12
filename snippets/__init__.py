f5d3dc2afd4e9e2ba1404451f68a288a24caf5fc
jan <152862650+j-haacker@users.noreply.github.com>
2025-08-12 15:51:20 +0200

    add is_notebook

--- snippets/__init__.py
+++ snippets/__init__.py
@@ -3,6 +3,7 @@
 __version__ = "0.1.0"
 
 __all__ = [
+    "is_notebook",
     "wrap_sys_exit",
     # modules
     "debugging",
@@ -14,6 +15,22 @@ __all__ = [
 
 from contextlib import contextmanager
 from sys import exit
+
+
+def is_notebook() -> bool:
+    # CREDIT Gustavo Bezerra https://stackoverflow.com/a/39662359
+    try:
+        shell = get_ipython().__class__.__name__  # type: ignore
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            print(f"`get_ipython().__class__.__name__` {shell} is not"
+                  "considered notebook.")
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
 
 
 @contextmanager
